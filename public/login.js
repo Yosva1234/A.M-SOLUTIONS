6c77993e30d2693a22dd2f61f67da294df48e96e
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2025-05-13 03:49:44 -0400

    3fweds
    edws

--- public/login.js
+++ public/login.js
@@ -1,20 +1,33 @@
 document.getElementById('login-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
-  
-    // Valores asignados (puedes cambiarlos)
-    const usuarioCorrecto = "yazmin";
-    const contraseñaCorrecta = "1234";
-  
-    // Obtener los valores ingresados por el usuario
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    // Validar el usuario y la contraseña
-    if (username === usuarioCorrecto && password === contraseñaCorrecta) {
-      // Redirigir a otra página si son correctos
-      window.location.href = "bienvenido.html"; // Cambia "bienvenido.html" por la página que desees
-    } else {
-      // Mostrar un mensaje de error si son incorrectos
-      alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
-    }
-  });
+  event.preventDefault(); 
+
+  const usuarioCorrecto = getelement('username');
+  const contraseñaCorrecta = getelement('password');
+
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === usuarioCorrecto && password === contraseñaCorrecta) {
+
+    window.location.href = "bienvenido.html"; 
+  } else {
+ 
+    alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
+  }
+});
+
+function getelement(element) {
+  return fetch(element)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => data.valor)
+      .catch(error => {
+          console.error('Hubo un problema con la solicitud:', error);
+      });
+}
+
