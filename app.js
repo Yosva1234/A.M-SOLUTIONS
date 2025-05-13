978e9f53e03bbc86bac91cc8ae64a59e430e9a21
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2025-05-13 01:11:25 -0400

    dd

--- app.js
+++ app.js
@@ -21,59 +21,51 @@ const pool = mysql.createPool({
 app.use(cors()); 
 app.use(express.json()); 
 
-// Servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Ruta para obtener todas las bebidas
-app.get('/productos', (req, res) => {
-  const query = 'SELECT * FROM productos'; // Consulta para obtener todas las bebidas
 
-  // Usar el pool para ejecutar la consulta
+app.get('/productos', (req, res) => {
+  const query = 'SELECT * FROM productos'; 
+
   pool.query(query, (err, results) => {
     if (err) {
       console.error('Error al ejecutar la consulta:', err.stack);
       res.status(500).send('Error en el servidor');
       return;
     }
-    res.json(results); // Enviar los resultados como JSON
+    res.json(results); 
   });
 });
 
-// Ruta para agregar una nueva bebida
-app.post('/productos', (req, res) => {
-  const { nombre, precio, info, imagen, varcategoria } = req.body; // Obtener los datos del cuerpo de la solicitud
 
-  // Validar que todos los campos estén presentes
+app.post('/productos', (req, res) => {
+  const { nombre, precio, info, imagen, varcategoria } = req.body; 
+
   if (!nombre  || !precio  || !info || !varcategoria ) {
     return res.status(400).json({ error: 'Todos los campos son obligatorios.' });
   }
 
-  const query = 'INSERT INTO productos (nombre, precio, info, imagen, varcategoria) VALUES (?, ? , ? , ? , ?)'; // Consulta para insertar una nueva bebida
+  const query = 'INSERT INTO productos (nombre, precio, info, imagen, varcategoria) VALUES (?, ? , ? , ? , ?)'; 
 
-  // Usar el pool para ejecutar la consulta
   pool.query(query, [nombre, precio, info, imagen, varcategoria], (err, results) => {
     if (err) {
       console.error('Error al agregar la los productos:', err.stack);
       return res.status(500).json({ error: 'Error en el servidor' });
     }
 
-    // Respuesta exitosa con el ID de la nueva bebida
     res.status(201).json({ message: 'producto agregado correctamente', id: results.insertId });
   });
 });
 
-// Ruta para eliminar una bebida por su ID
 app.delete('/productos/:id', (req, res) => {
-  const { id } = req.params; // Obtener el ID de la bebida desde los parámetros de la URL
+  const { id } = req.params;
 
-  const query = 'DELETE FROM productos WHERE id = ?'; // Consulta para eliminar la bebida
-
-  // Usar el pool para ejecutar la consulta
+  const query = 'DELETE FROM productos WHERE id = ?';
   pool.query(query, [id], (err, results) => {
     if (err) {
       console.error('Error al eliminar el productos:', err.stack);
@@ -82,22 +74,19 @@ app.delete('/productos/:id', (req, res) => {
     }
 
     if (results.affectedRows === 0) {
-      // Si no se eliminó ninguna fila, la bebida no existe
       res.status(404).json({ error: 'producto no encontrada' });
       return;
     }
 
-    res.json({ message: 'producto eliminada correctamente' }); // Respuesta exitosa
+    res.json({ message: 'producto eliminada correctamente' });
   });
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-// Cerrar el pool al detener la aplicación
 process.on('SIGINT', () => {
-  pool.end(); // Cerrar el pool de conexiones
-  process.exit(); // Salir de la aplicación
+  pool.end(); 
+  process.exit();
 });
